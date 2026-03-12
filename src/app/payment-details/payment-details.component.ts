bc411afc3f56436acfe4390e46fa333b8067d2cb
Alex-dev1356 <asoliven26@yahoo.com>
2026-03-13 05:17:36 +0800

    Injecting the service into payment-details component and using ngOnInit() to execute refreshList() method upon the component is called or executed

--- src/app/payment-details/payment-details.component.ts
+++ src/app/payment-details/payment-details.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailsService } from '../shared/payment-details.service';
 
 @Component({
   selector: 'app-payment-details',
   templateUrl: './payment-details.component.html',
   styleUrls: ['./payment-details.component.scss']
 })
-export class PaymentDetailsComponent {
+export class PaymentDetailsComponent implements OnInit {
+
+  constructor(public service:PaymentDetailsService) { }
+  ngOnInit(): void {
+    this.service.refreshList();
+  }
+
 
 }
