fbc08fba10e71bf9083953a337d02fb1d72841ed
Alex-dev1356 <asoliven26@yahoo.com>
2026-03-13 22:32:47 +0800

    Populating the Model of our UI using CamelCase with same spelling, same as the one on the API

--- src/app/shared/payment-detail.model.ts
+++ src/app/shared/payment-detail.model.ts
@@ -1,2 +1,7 @@
 export class PaymentDetail {
+    public paymentDetailId: number = 0
+    public carOwnerName: string = ''
+    public cardNumber: string = ''
+    public expirationDate: string = ''
+    public securityCode: string = ''
 }
