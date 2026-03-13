b5cee6339a8e78a844c42f85ebd7d2129487f535
Alex-dev1356 <asoliven26@yahoo.com>
2026-03-14 03:51:42 +0800

    Iterating through on the PaymentDetailList where the json response from the API is stored

--- src/app/shared/payment-detail.model.ts
+++ src/app/shared/payment-detail.model.ts
@@ -1,6 +1,6 @@
 export class PaymentDetail {
     public paymentDetailId: number = 0
-    public carOwnerName: string = ''
+    public cardOwnerName: string = ''
     public cardNumber: string = ''
     public expirationDate: string = ''
     public securityCode: string = ''
