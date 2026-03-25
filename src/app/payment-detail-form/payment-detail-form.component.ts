e0391a4a1cb208116ba3a10016371fa267970f61
Alex-dev1356 <asoliven26@yahoo.com>
2026-03-26 03:26:03 +0800

    Creating a Submit Event for the onSubmit method  and creating a variable that will cater the NgForm instance or object

--- src/app/payment-detail-form/payment-detail-form.component.ts
+++ src/app/payment-detail-form/payment-detail-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { PaymentDetailsService } from '../shared/payment-details.service';
+import { PaymentDetail } from '../shared/payment-detail.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-detail-form',
@@ -9,5 +11,7 @@ import { PaymentDetailsService } from '../shared/payment-details.service';
 export class PaymentDetailFormComponent {
 
   constructor(public service:PaymentDetailsService){}
+
+  onSubmit(form : NgForm){}
 
 }
